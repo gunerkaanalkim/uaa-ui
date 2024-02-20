c3dd4daba27b03accecd344952aec195e6d21ce0
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-02-20 20:57:05 +0000

    splash screen and login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services/authentication.service";
+import {Store} from "@ngrx/store";
+import {setUserInfo} from "../../store/project.action";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(
+    private readonly authenticationService: AuthenticationService,
+    private readonly store: Store,
+    private readonly router: Router
+  ) {
+    this.authenticationService = authenticationService;
+  }
 
+  username = new FormControl('kaanalkim', [Validators.minLength(5), Validators.maxLength(50)]);
+  password = new FormControl('1234', [Validators.minLength(8), Validators.maxLength(50)]);
+
+  onLogin() {
+    if (!this.formHasError()) {
+      this.authenticationService
+        .login(this.username.value!, this.password.value!)
+        .subscribe(response => {
+          this.store.dispatch(setUserInfo({userDetails: response}))
+          this.router.navigate(['home'])
+        })
+    }
+  }
+
+  usernameHasError() {
+    return this.username.invalid && (this.username.dirty || this.username.touched)
+  }
+
+  passwordHasError() {
+    return this.password.invalid && (this.password.dirty || this.password.touched)
+  }
+
+  formHasError() {
+    return this.usernameHasError() || this.passwordHasError();
+  }
 }
