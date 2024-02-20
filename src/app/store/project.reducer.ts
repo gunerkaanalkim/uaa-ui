df50f5ad6a6f673161f58ac29faeca108f5d3637
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-02-20 20:57:08 +0000

    splash screen and login

--- src/app/store/project.reducer.ts
+++ src/app/store/project.reducer.ts
@@ -2,7 +2,6 @@ import {on} from "@ngrx/store";
 import {createImmerReducer} from 'ngrx-immer/store';
 import {initialState} from "./initial-state";
 import * as ProjectViewActions from './project.action';
-import {checkUserLoggedIn} from "./project.action";
 
 
 export const mainReducer = createImmerReducer(
@@ -10,5 +9,8 @@ export const mainReducer = createImmerReducer(
   on(ProjectViewActions.checkUserLoggedIn, (state) => {
 
     return state;
-  })
+  }), on(ProjectViewActions.setUserInfo, (state, {userDetails}) => {
+    state.userDetails = userDetails;
+    return state;
+  }),
 );
