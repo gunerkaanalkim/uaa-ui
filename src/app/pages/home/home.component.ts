829cd37acd44bc577bd1b6e31b43b49d0d126947
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-02-22 21:50:17 +0000

    #3

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,33 @@
 import {Component, OnInit} from '@angular/core';
+import {ShopService} from "../../services/shop.service";
 import {Store} from "@ngrx/store";
-import {selectUserDetails} from "../../store/project.selector";
-import {UserDetails} from "../../store/model";
+import {setAllShops} from "../../store/project.action";
+import {Shop} from "../../store/model";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  shops: Shop[] = [];
+
   constructor(
+    private readonly shopService: ShopService,
+    private readonly store: Store
   ) {
+  }
+
+  ngOnInit(): void {
+    this.getAllShops();
+  }
+
+  getAllShops() {
+    this.shopService
+      .getAll()
+      .subscribe(shops => {
+        this.store.dispatch(setAllShops({shops: shops}))
+        this.shops = shops;
+      })
   }
 }
