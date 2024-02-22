0ff731ddee782ab88663be92283ad01beedef24f
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-02-22 21:50:32 +0000

    #3

--- src/app/services/shop.service.ts
+++ src/app/services/shop.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Shop} from "../store/model";
+import {catchError} from "rxjs";
+import {GlobalExceptionHandlerService} from "./global-exception-handler.service";
+import {APP_CONFIG} from "../config/tokens";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor() { }
+  constructor(
+    @Inject(APP_CONFIG) private config: any,
+    private readonly httpClient: HttpClient,
+    private readonly globalExceptionHandlerService: GlobalExceptionHandlerService,
+  ) {
+  }
+
+  getAll() {
+    return this.httpClient
+      .get<Shop[]>(`${this.config.api.services.integrator}${this.config.api.endpoints.getAllShops}`)
+      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+  }
+
+  create(shop: Shop) {
+    return this.httpClient
+      .post<Shop>(`${this.config.api.services.integrator}${this.config.api.endpoints.createShop}`, shop)
+      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+  }
 }
