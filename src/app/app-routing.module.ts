2a0772e958677880c98d7144736265cf13f219ba
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-02-22 21:50:09 +0000

    #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {LoginComponent} from "./pages/login/login.component";
 import {NotFoundComponent} from "./pages/not-found/not-found.component";
 import {HomeComponent} from "./pages/home/home.component";
 import {SplashComponent} from "./pages/splash/splash.component";
+import {NewShopComponent} from "./pages/shop/new-shop/new-shop.component";
+import {EditShopComponent} from "./pages/shop/edit-shop/edit-shop.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'splash', pathMatch: 'full'},
   {path: 'splash', component: SplashComponent},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'shop/new', component: NewShopComponent},
+  {path: 'shop/edit/:shopId', component: EditShopComponent},
   {path: '404', component: NotFoundComponent},
   {path: '**', component: NotFoundComponent}
 ];
