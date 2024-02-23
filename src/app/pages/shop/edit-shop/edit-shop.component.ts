7a3cc23969228d9043b2ed5363498bf479f80e12
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-02-23 22:45:35 +0000

    #3

--- src/app/pages/shop/edit-shop/edit-shop.component.ts
+++ src/app/pages/shop/edit-shop/edit-shop.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ShopService} from "../../../services/shop.service";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-shop',
@@ -7,15 +9,60 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./edit-shop.component.scss']
 })
 export class EditShopComponent implements OnInit {
+  shopTitle = new FormControl('', [Validators.minLength(5), Validators.maxLength(200)]);
+  shopDescription = new FormControl('', [Validators.minLength(5), Validators.maxLength(200)]);
+  etsyAccountID = new FormControl('', [Validators.minLength(5), Validators.maxLength(200)]);
+
   shopId!: number;
 
   constructor(
-    private activatedRoute: ActivatedRoute) {
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly shopService: ShopService,
+    private readonly router: Router
+  ) {
+  }
+
+  onEdit() {
+    if (!this.formHasError()) {
+      this.shopService
+        .edit({
+          title: this.shopTitle.value!,
+          description: this.shopDescription.value!,
+          etsyAccountId: this.etsyAccountID.value!,
+          id: this.shopId
+        })
+        .subscribe(shop => {
+          this.router.navigate(['home'])
+        })
+    }
   }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(({shopId}) => {
-      this.shopId = shopId;
+      this.shopService
+        .getById(shopId)
+        .subscribe((shop) => {
+          this.shopId = shop.id;
+          this.shopTitle.setValue(shop.title);
+          this.shopDescription.setValue(shop.description);
+          this.etsyAccountID.setValue(shop.etsyAccountId);
+        })
     })
+  }
+
+  titleHasError() {
+    return this.shopTitle.invalid && (this.shopTitle.dirty || this.shopTitle.touched)
+  }
+
+  descriptionHasError() {
+    return this.shopDescription.invalid && (this.shopDescription.dirty || this.shopDescription.touched)
+  }
+
+  etsyAccountIDHasError() {
+    return this.etsyAccountID.invalid && (this.etsyAccountID.dirty || this.etsyAccountID.touched)
+  }
+
+  formHasError() {
+    return this.titleHasError() || this.descriptionHasError() || this.etsyAccountIDHasError();
   }
 }
