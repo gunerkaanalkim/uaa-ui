3501e86d4ed942bd87461ea42d954ada24e0c807
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-02-23 22:45:37 +0000

    #3

--- src/app/shared-components/header/header.component.ts
+++ src/app/shared-components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserDetails} from "../../store/model";
 import {Store} from "@ngrx/store";
 import {LOGOUT} from "../../store/project.action";
