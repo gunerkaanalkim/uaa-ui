7025b50de6011c352532171399e367fb1669ffc6
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-05 17:15:46 +0000

    #4

--- src/app/pages/provider/provider-detail/provider-detail.component.ts
+++ src/app/pages/provider/provider-detail/provider-detail.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
+import {IntegrationService} from "../../../services/integration.service";
+import {Brand, Product} from "../../../store/model";
 
 @Component({
   selector: 'app-provider-detail',
@@ -8,18 +10,37 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class ProviderDetailComponent implements OnInit {
   providerAlias: string = '';
+  brands: Brand[] = [];
+  products: Product[] = [];
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly router: Router,
+    private readonly integrationService: IntegrationService
   ) {
-
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.providerAlias = params['providerAlias'];
+
+      this.getAllBrands(this.providerAlias);
     });
+  }
+
+  getAllBrands(providerAlias: string) {
+    this.integrationService
+      .getAllBrands(providerAlias)
+      .subscribe(brands => {
+        this.brands = brands;
+      })
+  }
+
+  getProductsByBrand(brandId: number) {
+    this.integrationService
+      .getProductsByBrand(this.providerAlias, brandId)
+      .subscribe(products => {
+        this.products = products;
+      })
   }
 
 }
