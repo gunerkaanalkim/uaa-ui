9ab65a6590c59c5944d11d9197c3a27bc12025df
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-05 17:15:35 +0000

    #4

---
+++ src/app/shared-components/brand/brand.component.ts
@@ -0,0 +1,16 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Brand} from "../../store/model";
+
+@Component({
+  selector: 'app-brand',
+  templateUrl: './brand.component.html',
+  styleUrls: ['./brand.component.scss']
+})
+export class BrandComponent {
+  @Input() brands: Brand[] = [];
+  @Output() onClick = new EventEmitter();
+
+  _getProducts(brandId: number) {
+    this.onClick.emit(brandId);
+  }
+}
