c696f4fe0f1e7a5f3e1ce92c5020e9fd4f675703
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-06 23:14:19 +0000

    #4

---
+++ src/app/shared-components/pager/pager.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Pagination} from "../../store/model";
+
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss']
+})
+export class PagerComponent implements OnInit {
+  @Input() total: number = 0;
+  @Input() currentPage: number = 0;
+  @Input() perPage: number = 0;
+  @Input() lastPage: number = 0;
+  @Output() onSelect = new EventEmitter();
+  datasource: number[] = [];
+
+  ngOnInit(): void {
+    this.datasource = Array(this.lastPage);
+  }
+
+  normalizeIndex(index: number) {
+    return ++index;
+  }
+
+  _onClick(event: any) {
+    this.onSelect.emit(event.target.value);
+  }
+}
