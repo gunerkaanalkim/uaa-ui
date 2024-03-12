3a448b4fcdc1045a9540c335a6005917bde96e63
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-12 23:15:25 +0000

    #4

---
+++ src/app/pages/product-db/product-db.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-product-db',
+  templateUrl: './product-db.component.html',
+  styleUrls: ['./product-db.component.scss']
+})
+export class ProductDBComponent implements OnInit{
+  constructor(
+    private readonly productService: ProductService
+  ) {
+  }
+
+  ngOnInit(): void {
+      this.productService
+        .getAll()
+        .subscribe(response=>{
+          console.log(response)
+        })
+  }
+
+
+}
