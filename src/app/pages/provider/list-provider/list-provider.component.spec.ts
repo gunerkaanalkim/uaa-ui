ac33d980103f5909ad7614e0f6858384913220b8
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-13 21:45:26 +0000

    #4

--- src/app/pages/provider/list-provider/list-provider.component.spec.ts
+++ src/app/pages/provider/list-provider/list-provider.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ListProviderComponent } from './list-provider.component';
+import {ListProviderComponent} from './list-provider.component';
 
 describe('ListProviderComponent', () => {
   let component: ListProviderComponent;
