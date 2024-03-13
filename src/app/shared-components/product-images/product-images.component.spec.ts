b4e553848ca3c5c5d4e0d03edc3f1b037d41d4bf
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-13 21:45:33 +0000

    #4

--- src/app/shared-components/product-images/product-images.component.spec.ts
+++ src/app/shared-components/product-images/product-images.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProductImagesComponent } from './product-images.component';
+import {ProductImagesComponent} from './product-images.component';
 
 describe('ProductImagesComponent', () => {
   let component: ProductImagesComponent;
