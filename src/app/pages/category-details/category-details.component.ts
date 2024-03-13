3ddb67abd83f920ab3c9c774e24f633410ebd27b
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-13 21:45:22 +0000

    #4

--- src/app/pages/category-details/category-details.component.ts
+++ src/app/pages/category-details/category-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ProductResponse} from "../../store/model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {IntegrationService} from "../../services/integration.service";
