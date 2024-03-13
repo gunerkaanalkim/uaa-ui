08891ba6c274ab7766e049daa72d96331d534645
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-13 21:45:19 +0000

    #4

--- src/app/pages/brand-detail/brand-detail.component.spec.ts
+++ src/app/pages/brand-detail/brand-detail.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BrandDetailComponent } from './brand-detail.component';
+import {BrandDetailComponent} from './brand-detail.component';
 
 describe('BrandDetailComponent', () => {
   let component: BrandDetailComponent;
