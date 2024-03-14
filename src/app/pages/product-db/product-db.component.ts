51dbbbd1f0d01cec80456be6ccbf4418dcc073ee
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-14 21:23:00 +0000

    #4

--- src/app/pages/product-db/product-db.component.ts
+++ src/app/pages/product-db/product-db.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
+import {Store} from "@ngrx/store";
+import {setLoaderVisible} from "../../store/project.action";
+import {Product, ProductImage} from "../../store/model";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-db',
@@ -7,19 +11,54 @@ import {ProductService} from "../../services/product.service";
   styleUrls: ['./product-db.component.scss']
 })
 export class ProductDBComponent implements OnInit{
+  products: Product[] = [];
+  productImages: ProductImage[] = [];
+  pageable: any;
+  page: number = 1;
+
   constructor(
-    private readonly productService: ProductService
+    private readonly productService: ProductService,
+    private readonly store: Store,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
   ) {
   }
 
   ngOnInit(): void {
-      this.productService
-        .getAll()
-        .subscribe(response=>{
-          console.log(response)
-        })
+    this.route.params.subscribe(params => {
+      this.page = params['page'];
+
+      this.getAllProductsByPage(this.page);
+    });
+
   }
 
+  getAllProductsByPage(pageNo: number) {
+    this.store.dispatch(setLoaderVisible({isLoaderVisible: true}))
 
+    this.productService
+      .getAll(pageNo)
+      .subscribe(pageableProducts => {
+        this.store.dispatch(setLoaderVisible({isLoaderVisible: false}))
+        this.products = pageableProducts.content;
+        this.pageable = {
+          currentPage: pageableProducts.pageable.pageNumber,
+          lastPage: pageableProducts.totalPages,
+          perPage: pageableProducts.pageable.pageSize,
+          total: pageableProducts.totalElements
+        }
+      })
+  }
 
+  onPageSelect(page: number) {
+    this.router.navigate(['product-database', {page: page}])
+  }
+
+  onImagesOpen(productId: number) {
+    this.productService
+      .getProductImages(productId)
+      .subscribe(images=>{
+        this.productImages = images;
+      })
+  }
 }
