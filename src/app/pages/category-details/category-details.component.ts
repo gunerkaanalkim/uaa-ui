88170ba2e5269040fb6454173cf1c386b749c645
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-15 16:39:03 +0000

    environments for production

--- src/app/pages/category-details/category-details.component.ts
+++ src/app/pages/category-details/category-details.component.ts
@@ -1,25 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductResponse} from "../../store/model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {IntegrationService} from "../../services/integration.service";
-import {Store} from "@ngrx/store";
+import {NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-category-details',
   templateUrl: './category-details.component.html',
   styleUrls: ['./category-details.component.scss']
 })
-export class CategoryDetailsComponent {
+export class CategoryDetailsComponent implements OnInit {
   providerAlias: string = '';
   page: number = 1;
   categoryId!: number;
+  categoryName!: string;
   productResponse: ProductResponse | null = null;
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly integrationService: IntegrationService,
-    private readonly store: Store
+    private spinner: NgxSpinnerService
   ) {
   }
 
@@ -28,19 +29,20 @@ export class CategoryDetailsComponent {
       this.providerAlias = params['providerAlias'];
       this.page = params['page'];
       this.categoryId = params['categoryId'];
+      this.categoryName = params['categoryName'];
 
       this.getProductsByBrand(this.categoryId);
     });
   }
 
   getProductsByBrand(brandId: number) {
-
+    this.spinner.show();
 
     this.integrationService
       .getProductsByCategory(this.providerAlias, brandId, this.page)
       .subscribe(productResponse => {
         this.productResponse = productResponse;
-
+        this.spinner.hide();
       })
   }
 
