51b3f83a92fb1088ace6fb186526a655f12b45cc
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-19 21:23:40 +0000

    environments for production

--- src/app/services/integration.service.ts
+++ src/app/services/integration.service.ts
@@ -4,7 +4,7 @@ import {HttpClient} from "@angular/common/http";
 import {GlobalExceptionHandlerService} from "./global-exception-handler.service";
 import {
   Brand,
-  Category,
+  Category, GenerateContentRequest, GenerateContentResponse,
   Product,
   ProductImage,
   ProductResponse,
@@ -12,6 +12,9 @@ import {
   ProductVariantOption
 } from "../store/model";
 import {catchError} from "rxjs";
+import {Store} from "@ngrx/store";
+import {NgxSpinnerService} from "ngx-spinner";
+import {setHttpError} from "../store/project.action";
 
 @Injectable({
   providedIn: 'root'
@@ -22,31 +25,46 @@ export class IntegrationService {
     @Inject(APP_CONFIG) private config: any,
     private readonly httpClient: HttpClient,
     private readonly globalExceptionHandlerService: GlobalExceptionHandlerService,
+    private readonly store: Store,
+    private spinner: NgxSpinnerService
   ) {
+    this.store.dispatch(setHttpError({httpError: null}))
   }
 
   getAllBrands(providerAlias: string) {
     return this.httpClient
       .get<Brand[]>(`${this.config.api.services.integrator}${this.config.api.endpoints.getAllBrands}/${providerAlias}`)
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 
   getProductsByBrand(providerAlias: string, brandId: number, page: number) {
     return this.httpClient
       .get<ProductResponse>(`${this.config.api.services.integrator}${this.config.api.endpoints.getProductsByBrand}/${providerAlias}/${brandId}/${page}`)
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 
   getAllCategories(providerAlias: string) {
     return this.httpClient
       .get<Category[]>(`${this.config.api.services.integrator}${this.config.api.endpoints.getAllCategories}/${providerAlias}`)
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 
   getProductsByCategory(providerAlias: string, categoryId: number, page: number) {
     return this.httpClient
       .get<ProductResponse>(`${this.config.api.services.integrator}${this.config.api.endpoints.getProductsByCategory}/${providerAlias}/${categoryId}/${page}`)
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 
   addToProductDb(providerAlias: string, productId: number, shopId: number) {
@@ -55,7 +73,10 @@ export class IntegrationService {
         productId: productId,
         shopId: shopId
       })
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 
   saveProductImages(productId: number, images: ProductImage[]) {
@@ -64,7 +85,10 @@ export class IntegrationService {
         productId: productId,
         images: images
       })
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 
   saveProductVariants(productId: number, variants: ProductVariant[]) {
@@ -73,7 +97,10 @@ export class IntegrationService {
         productId: productId,
         variants: variants
       })
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 
   saveProductVariantOptions(productId: number, providerAlias: string) {
@@ -82,6 +109,20 @@ export class IntegrationService {
         productId: productId,
         providerAlias: providerAlias
       })
-      .pipe(catchError(this.globalExceptionHandlerService.handleError));
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
+  }
+
+  generateContent(generateContentRequest: GenerateContentRequest) {
+    return this.httpClient
+      .post<GenerateContentResponse>(`${this.config.api.services.integrator}${this.config.api.endpoints.generateContent}`, {
+        ...generateContentRequest
+      })
+      .pipe(catchError(this.globalExceptionHandlerService.handleError.bind({
+        store: this.store,
+        spinner: this.spinner
+      })))
   }
 }
