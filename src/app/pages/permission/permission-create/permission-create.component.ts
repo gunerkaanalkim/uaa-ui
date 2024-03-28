166a78785d410df38190bcf5c8dbac789ec7270b
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-03-28 20:21:46 +0000

    dockerizing

---
+++ src/app/pages/permission/permission-create/permission-create.component.ts
@@ -0,0 +1,74 @@
+import {Component} from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {PermissionService} from "../../../services/permission.service";
+
+@Component({
+  selector: 'app-permission-create',
+  templateUrl: './permission-create.component.html',
+  styleUrls: ['./permission-create.component.scss']
+})
+export class PermissionCreateComponent {
+  permissionName = new FormControl('',
+    [Validators.minLength(5), Validators.maxLength(200)]
+  );
+
+  permissionDescription = new FormControl('',
+    [Validators.minLength(5), Validators.maxLength(200)]
+  );
+
+  permissionURL = new FormControl('',
+    [Validators.minLength(5), Validators.maxLength(200)]
+  );
+
+  permissionController = new FormControl('',
+    [Validators.minLength(5), Validators.maxLength(200)]
+  );
+
+  permissionId!: number;
+
+  constructor(
+    private readonly service: PermissionService,
+    private readonly router: Router
+  ) {
+  }
+
+  onEdit() {
+    if (!this.formHasError()) {
+      this.service
+        .create({
+          id: this.permissionId,
+          url: this.permissionURL.value!,
+          title: this.permissionName.value!,
+          description: this.permissionDescription.value!,
+          controller: this.permissionController.value!
+        })
+        .subscribe(shop => {
+          this.router.navigate(['/admin/permission/list', {pageNo: 1}])
+        })
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  permissionNameHasError() {
+    return this.permissionName.invalid && (this.permissionName.dirty || this.permissionName.touched)
+  }
+
+  permissionDescriptionHasError() {
+    return this.permissionDescription.invalid && (this.permissionDescription.dirty || this.permissionDescription.touched)
+  }
+
+  permissionURLHasError() {
+    return this.permissionURL.invalid && (this.permissionURL.dirty || this.permissionURL.touched)
+  }
+
+  permissionControllerHasError() {
+    return this.permissionController.invalid && (this.permissionController.dirty || this.permissionController.touched)
+  }
+
+  formHasError() {
+    return this.permissionNameHasError() || this.permissionDescriptionHasError();
+  }
+}
